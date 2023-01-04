4b4cd462819c8eb8a08841529416b056a5cb542d
Kyle Belanger <kyleb44@hotmail.com>
2023-01-04 08:17:55 -0500

    Create 0-greeter-dbcreation.R

---
+++ ML/0-greeter-dbcreation.R
@@ -0,0 +1,3 @@
+# The follow script rebuilds a local copy of the MIMIC-IV database
+# for the purpose of this project only the patients, labitems, and labevents
+# tables are included, and the csv's need to be in the data-unshared folder
