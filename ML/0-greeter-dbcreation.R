6f9cbf41e66117974ed3126d1990944952edc4e3
Kyle Belanger <kyleb44@hotmail.com>
2023-01-04 08:27:49 -0500

    Update 0-greeter-dbcreation.R

--- ML/0-greeter-dbcreation.R
+++ ML/0-greeter-dbcreation.R
@@ -1,3 +1,14 @@
 # The follow script rebuilds a local copy of the MIMIC-IV database
 # for the purpose of this project only the patients, labitems, and labevents
 # tables are included, and the csv's need to be in the data-unshared folder
+# this script only needs to be run once on any computer performing analysis
+
+rm(list = ls(all.names = TRUE)) # Clear the memory of variables from previous run.
+cat("\014") # Clear the console
+
+
+# load packages -----------------------------------------------------------
+
+box::use(
+  magrittr[`%>%`]
+)
